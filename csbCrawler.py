79c81603b1b8de888812deb24a2401794a8fef32
dneufeld <david.neufeld@colorado.edu>
2019-09-13 12:20:03 -0600

    Initial import.

---
+++ csbCrawler.py
@@ -0,0 +1,37 @@
+# recurse_dir.py
+import os
+import tarfile
+
+# Extract item
+def extract_item(file_item):
+    print ("Extracting..." + file_item)
+    tar = tarfile.open(file_item, "r:gz")
+    for tarinfo in tar:
+        print (tarinfo.name, "is", tarinfo.size, "bytes in size and is")
+        if tarinfo.isreg():
+            print("a regular file.")
+        elif tarinfo.isdir():
+            print("a directory.")
+        else:
+            print("something else.")
+    tar.close()
+
+
+# Print every file with its size recursing through dirs
+def recurse_dir(root_dir):
+    root_dir = os.path.abspath(root_dir)
+    for item in os.listdir(root_dir):
+        item_full_path = os.path.join(root_dir, item)
+
+        if os.path.isdir(item_full_path):
+            recurse_dir(item_full_path)
+        else:
+            if item[6 :]== ".tar.gz":
+                print("%s - %s bytes" % (item_full_path, os.stat(item_full_path).st_size))
+                extract_item(item_full_path)
+
+if __name__ == '__main__':
+    # Get full size of home directory
+    ## Switch to config file for data directory
+    recurse_dir(r"/Users/dneufeld/data")
+
