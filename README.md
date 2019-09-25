ae9c490f6fbfde3d229df70a36a8285e53e20262
dneufeld <david.neufeld@colorado.edu>
2019-09-25 14:35:01 -0600

    Only extract xyz and metadata json files.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # csbCrawler2Cloud
-Python load to s3 buckets
+Python script to load csb data to s3 buckets
+
+Going forward the data lands on NCEI disk as a tarball with 3 files:
+ - YYYYMMDD_uuid_geojson.json
+ - YYYYMMDD_uuid_metadata.json
+ - YYYYMMDD_uuid_pointData.xyz
+ 
+ 
+ 
+ 
--- csbCrawler.py
+++ csbCrawler.py
@@ -4,14 +4,14 @@ import tarfile
 
 # Extract item
 def extract_item(file_item):
-    print ("Extracting..." + file_item)
     tar = tarfile.open(file_item, "r:gz")
     for tarinfo in tar:
-        print (tarinfo.name, "is", tarinfo.size, "bytes in size and is")
+        #print(tarinfo.name, "is", tarinfo.size, "bytes in size and is")
 
         if tarinfo.isreg():
-            print("a regular file.")
-            tar.extract(tarinfo, r"/tmp", True)
+            if tarinfo.name[-4:]== ".xyz" or tarinfo.name[-13:]== "metadata.json":
+                print("extracting... " + tarinfo.name)
+                tar.extract(tarinfo, "output", True)
     tar.close()
 
 
@@ -24,12 +24,12 @@ def recurse_dir(root_dir):
         if os.path.isdir(item_full_path):
             recurse_dir(item_full_path)
         else:
-            if item[6 :]== ".tar.gz":
+            if item[-7:] == ".tar.gz":
                 print("%s - %s bytes" % (item_full_path, os.stat(item_full_path).st_size))
                 extract_item(item_full_path)
 
 if __name__ == '__main__':
     # Get full size of home directory
     ## Switch to config file for data directory
-    recurse_dir(r"/Users/david.neufeld/data")
+    recurse_dir("data")
 
