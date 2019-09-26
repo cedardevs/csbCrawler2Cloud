4914f72401004ccd1bd8fcb73a703444c7a281c5
dneufeld <david.neufeld@colorado.edu>
2019-09-26 16:29:57 -0600

    Moving towards in-memory operations on the tar file to limit io.

--- app/csbCrawler.py
+++ app/csbCrawler.py
@@ -1,23 +1,28 @@
 # Extract CSB files
 
+import io
 import json
 import os
 import tarfile
 
-def parse_metadata(file_item):
-    return []
+def parse_metadata(file_name):
+    with open(file_name, "r") as read_file:
+        data = json.load(read_file)
+    return data
 
 # Extract item
-def extract_item(file_item):
-    tar = tarfile.open(file_item, "r:gz")
-    for tarinfo in tar:
+def extract_item(item_full_path):
+    tar = tarfile.open(item_full_path, "r:gz")
+    for tar_info in tar:
         #print(tarinfo.name, "is", tarinfo.size, "bytes in size and is")
 
-        if tarinfo.isreg():
-            if tarinfo.name[-4:]== ".xyz" or tarinfo.name[-13:]== "metadata.json":
-                print("extracting... " + tarinfo.name)
-                tar.extract(tarinfo, "output", True)
+        if tar_info.isreg() and (tar_info.name[-4:] == ".xyz" or tar_info.name[-13:] == "metadata.json"):
+            if tar_info.name[-4:] == ".xyz" or tar_info.name[-13:] == "metadata.json":
+                print("extracting... " + tar_info.name)
+                file = tar.extractfile(tar_info)
+                print(file.readline())
     tar.close()
+    return file
 
 
 # Print every file with its size recursing through dirs
@@ -31,7 +36,8 @@ def recurse_dir(root_dir):
         else:
             if item[-7:] == ".tar.gz":
                 print("%s - %s bytes" % (item_full_path, os.stat(item_full_path).st_size))
-                extract_item(item_full_path)
+                csv_input = extract_item(item_full_path)
+                print (csv_input.readline())
 
 if __name__ == '__main__':
     # Get full size of home directory
