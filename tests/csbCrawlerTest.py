cd4ae7b9e55c1a8e83b95e41f4dbea025e4e748a
dneufeld <david.neufeld@colorado.edu>
2019-09-26 16:30:26 -0600

    And proper assert statements to the test.

--- tests/csbCrawlerTest.py
+++ tests/csbCrawlerTest.py
@@ -3,5 +3,9 @@ import app.csbCrawler
 
 class csbCrawlerTest(unittest.TestCase):
     def test_parse_metadata(self):
-        print(app.csbCrawler.parse_metadata())
+        file_name = r"../data/20190626_8bfee6d7ec345d3b503a4ed3adc0288b_metadata.json"
+        data = app.csbCrawler.parse_metadata(file_name)
+        self.assertTrue(data['platform']['name'], "Joe Pyne")
+        self.assertTrue(data['platform']['uniqueID'], "ROSEP-ffa635f8-7aa4-49d8-bc04-581457fb9e46")
+        self.assertTrue(data['providerContactPoint']['orgName'], "Rose Point")
 
