65b74e5384285fd97311c004bb3e4c698cb9ec4f
David Neufeld <david.neufeld@localhost>
2020-06-11 17:49:25 -0600

    Add exception handler to allow continued processing in case of spatial join failure

--- app/spatialutil.py
+++ app/spatialutil.py
@@ -9,22 +9,27 @@ import shapely.geometry as geom
 
 # Add attributes of polygons to points file
 def spatial_join(csb_crawler, pts_file):
-    # Read CSV file into a dataframe
-    df = pd.read_csv(pts_file)
+    try:
 
-    poly_file = csb_crawler.test_data_dir + "gis/TERRITORIAL_SEAS.shp"
-    # Read shapefile into geodataframe
-    data_gdf = gpd.read_file(poly_file)
+        # Read CSV file into a dataframe
+        df = pd.read_csv(pts_file)
 
-    # Remove dups and spatially enable points
-    df.drop_duplicates(['UUID', 'LON', 'LAT', 'DEPTH','PLATFORM_NAME','PROVIDER'], inplace=True)
-    geometry = [geom.Point(xy) for xy in zip(df.LON, df.LAT)]
-    crs = "epsg:4326"  # http://www.spatialreference.org/ref/epsg/4326/
-    points_gdf = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
+        poly_file = csb_crawler.test_data_dir + "gis/TERRITORIAL_SEAS.shp"
+        # Read shapefile into geodataframe
+        data_gdf = gpd.read_file(poly_file)
 
-    # Create a subset of with just country name and exclude columns
-    sub_gdf = data_gdf[['EXCLUDE', 'geometry']]
+        # Remove dups and spatially enable points
+        df.drop_duplicates(['UUID', 'LON', 'LAT', 'DEPTH', 'PLATFORM_NAME', 'PROVIDER'], inplace=True)
+        geometry = [geom.Point(xy) for xy in zip(df.LON, df.LAT)]
+        crs = "epsg:4326"  # http://www.spatialreference.org/ref/epsg/4326/
+        points_gdf = gpd.GeoDataFrame(df, crs=crs, geometry=geometry)
 
-    # Try spatial join
-    join = gpd.sjoin(points_gdf, sub_gdf, how="left", op="within")
-    return join
+        # Create a subset of with just country name and exclude columns
+        sub_gdf = data_gdf[['EXCLUDE', 'geometry']]
+
+        # Try spatial join
+        join = gpd.sjoin(points_gdf, sub_gdf, how="left", op="within")
+        return join
+
+    except:
+        return None
